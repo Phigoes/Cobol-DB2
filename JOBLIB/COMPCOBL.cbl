53d1698647ed806b1420faab117cc03ba4be0837
Phigoes <phi_go@outlook.com>
2016-08-23 00:44:41 -0300

    Compilador Cobol

---
+++ JOBLIB/COMPCOBL.cbl
@@ -0,0 +1,11 @@
+//EAD316CB JOB 'EAD316',CLASS=C,MSGCLASS=X,NOTIFY=&SYSUID
+//************************************************************
+//*  NOME  = SEU PROGRAMA                                    *
+//*  LIB1  = SEU CHAVE                                       *
+//*  NAME  - SEU PROGRAMA                                    *
+//************************************************************
+// JCLLIB  ORDER=GR.GERAL.PROCLIB
+//COBCOMP  EXEC DFHCOBLB,LIB1=GR.EAD316,NOME='NOMEPROGRAMA',LIB2=GR.GERAL
+//LKED.SYSIN    DD   *
+  NAME NOMEPROGRAMA(R)
+/*  
